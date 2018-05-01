eb42c99e1751a5be67aa5360690bd54ba11570fb
Matthew Wolff <mm.wolff@chi-squared.org>
2018-05-01 17:02:22 -0500

    added descriptive comment

--- transfer.sh
+++ transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `ls -Sr1 *.tgz`; do
+for i in `ls -Sr1 *.tgz`; do # transfer all of the tgz files in current directory to gluster
   if grep -q "$i" /Volumes/mcmahonlab/home/mwolff3/processed.txt; then
     echo "already processed $i"
   else
