fbb05176fbf1476d03a68b3bc3f483975ddbcfe8
Matthew Wolff <mm.wolff@chi-squared.org>
2018-05-01 17:39:24 -0500

    added ~some~ parallelism

--- coalesce_script.sh
+++ coalesce_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # decompress all fasta files in parallel
-parallel --jobs 0 tar -xzf ::: /mnt/gluster/mwolff3/*fasta.tgz echo "decompression successful" # jobs 0 will run as many jobs in parallel as possible
+for f in `ls -Sr1 /mnt/gluster/mwolff3/*fasta.tgz`; do
+  tar -xzf /mnt/gluster/mwolff3/$f &
+done
+wait && echo "decompression successful"
 
 # remove the compressed versions
-parallel --jobs 0 rm -f ::: *fasta.tgz && echo "removal of compressed files successful"
+rm -f *fasta.tgz && echo "removal of compressed files successful"
 
 # compress output into an archive in Gluster, removing from current
 tar --remove-files -czf /mnt/gluster/mwolff3/metaGenomes.tar.gz *fasta && echo "tarring successful" && ls
