c7772c9789239ecd98e5dd6a58af9014c42fcfb3
Matthew Wolff <mwolff3@wisc.edu>
2018-05-07 18:05:34 -0500

    made wording consistent

--- Mapping/mapping_script.sh
+++ Mapping/mapping_script.sh
@@ -22,7 +22,7 @@ for f in `ls /mnt/gluster/mwolff3/*.fasta.tgz`; do
   [[ $f = *"PUBG"* ]] && continue
   [[ $f = *"PUBN"* ]] && continue
   tar -xzf $f -C metagenomes/
-done && echo "retrieved metagenomes"
+done && echo "extracted metagenomes"
 # tar -xzf /mnt/gluster/mwolff3/metagenomes.tar.gz -C metagenomes/ && echo "extracted metagenomes"
 tar -xzf /mnt/gluster/mwolff3/refGenomes.tar.gz -C refGenomes/ && echo "extracted refGenomes"
 echo
